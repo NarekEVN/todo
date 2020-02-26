2459929749034bf3b865dc5f12c5c7f2eddc9978
Narek <narekvoskanyan3@gmail.com>
2020-02-26 18:43:29 +0400

    script src

---
+++ build/precache-manifest.379c0538facb9254720dd7a015bfa446.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd3d07eac309c672012dce0db7272540",
+    "url": "/NarekEVN/todo/index.html"
+  },
+  {
+    "revision": "d4f305d739f414195081",
+    "url": "/NarekEVN/todo/static/css/main.6c0ac2a6.chunk.css"
+  },
+  {
+    "revision": "e34e0e351986337ac0db",
+    "url": "/NarekEVN/todo/static/js/2.9bb6dbd0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/NarekEVN/todo/static/js/2.9bb6dbd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4f305d739f414195081",
+    "url": "/NarekEVN/todo/static/js/main.85fe67b2.chunk.js"
+  },
+  {
+    "revision": "fe596d49f12b54380b80",
+    "url": "/NarekEVN/todo/static/js/runtime-main.1faa5a41.js"
+  }
+]);
